69e48073e48ae7ceaa464382d694d485b7b1bc51
IOSdeVeloper20 <81517945+IOSdeVeloper20@users.noreply.github.com>
2023-06-01 19:52:05 +0300

    add things

---
+++ Pods/Target Support Files/Pods-Smart Farmer/Pods-Smart Farmer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Smart_Farmer : NSObject
+@end
+@implementation PodsDummy_Pods_Smart_Farmer
+@end
--- Smart Farmer/Controller/Tabs/Home/HomeVC.swift
+++ Smart Farmer/Controller/Tabs/Home/HomeVC.swift
@@ -9,45 +9,24 @@ import UIKit
 
 class HomeVC: UIViewController {
 
-    let shapeLayer = CAShapeLayer()
-    let trackLayer = CAShapeLayer()
-
+//MARK: IBOutlets
+    
+    @IBOutlet weak var waterPercentageLbl: UILabel!
+    
+    @IBOutlet weak var waterLvlSlider: UISlider!
+    
+    @IBOutlet weak var tempraturePercentageLbl: UILabel!
+    
+    @IBOutlet weak var tempratureLvlSlider: UISlider!
+    
+    @IBOutlet weak var lightImage: UIImageView!
+    
+    //MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let center = view.center
+    }
 
-        //draw trach
-        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
-        trackLayer.path = circularPath.cgPath
-        trackLayer.strokeColor = UIColor.lightGray.cgColor
-        trackLayer.lineWidth = 10
-        trackLayer.fillColor = UIColor.clear.cgColor
-        trackLayer.lineCap = CAShapeLayerLineCap.round
-        view.layer.addSublayer(trackLayer)
-        
-        //draw a circle
-//        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
-        shapeLayer.path = circularPath.cgPath
-        shapeLayer.strokeColor = UIColor.green.cgColor
-        shapeLayer.lineWidth = 10
-        shapeLayer.strokeEnd = 0
-        shapeLayer.fillColor = UIColor.clear.cgColor
-        shapeLayer.lineCap = CAShapeLayerLineCap.round
-        view.layer.addSublayer(shapeLayer)
-        
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleProgressTap)))
-    }
     
-    @objc private func handleProgressTap() {
-        print("heloo")
-        let basicanimation = CABasicAnimation(keyPath: "strokeEnd")
-        
-        basicanimation.toValue = 1
-        basicanimation.duration = 2
-        //to keep the animation after it ends
-        basicanimation.fillMode = CAMediaTimingFillMode.forwards
-        basicanimation.isRemovedOnCompletion = false
-        shapeLayer.add(basicanimation, forKey: "basic")
-    }
+//MARK: Functions
+    
 }
